39395e89232af74217af706ad55580653f573036
Grant Groseclose <grantgroseclose@gmail.com>
2024-06-21 06:59:04 -0400

    add number preprocess within addTask schema

--- types/AddTaskData.ts
+++ types/AddTaskData.ts
@@ -4,9 +4,18 @@ import { z } from 'zod';
 
 const validationSchema = z.object({
     title: z.string().min(1),
-    deadline: z.string().min(1),
-    priority: z.string().min(1),
-    executionTime: z.string().min(1),
+    deadline: z.preprocess(
+        (a) => parseInt(z.string().parse(a), 10),
+        z.number().positive().min(1)
+    ),
+    priority: z.preprocess(
+        (a) => parseInt(z.string().parse(a), 10),
+        z.number().positive().min(1)
+    ),
+    executionTime: z.preprocess(
+        (a) => parseInt(z.string().parse(a), 10),
+        z.number().positive().min(1)
+    )
 });
   
 type AddTaskFormSubmissionData = z.infer<typeof validationSchema> & FieldValues;
