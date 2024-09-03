4f67129535fccf4bdffb04ee6ed7a88ada3f1d5e
Grant Groseclose <grantgroseclose@gmail.com>
2024-09-02 23:59:44 -0400

    make AuthStore login return type consistent w/ AuthService definitions

--- store/auth.ts
+++ store/auth.ts
@@ -1,10 +1,10 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-import { useLoginService } from "../services/AuthService";
+import { LoginClientData, useLoginService } from "../services/AuthService";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ExistingUser } from "../types/data/ExistingUser";
-import { z, ZodString } from "zod";
+import { z } from "zod";
 
 
 
@@ -14,7 +14,7 @@ interface AuthStore {
     firstname: string;
     isAuth: boolean;
     error: string;
-    login: (data: ExistingUser) => Promise<{ token: string, firstname: string } | { error: string } | void>;
+    login: (data: ExistingUser) => Promise<z.infer<LoginClientData['response']> | void>;
     logout: () => void;
 }
 
@@ -30,27 +30,23 @@ export const useAuthStore = create(
                 const LoginService = useLoginService();
                 const res = await LoginService.post(data);
 
-                // switch (typeof res) {
-                //     case 'object': {
-                        if ('error' in res) {
-                            set((state) => ({ 
-                                token: "",
-                                firstname: "",
-                                isAuth: !res.error,
-                                error: res.error
-                            }));
+                if ('error' in res) {
+                    set((state) => ({ 
+                        token: "",
+                        firstname: "",
+                        isAuth: !res.error,
+                        error: res.error
+                    }));
 
-                            return res;
-                        } else {
-                            set((state) => ({ 
-                                token: res.token,
-                                firstname: res.firstname,
-                                isAuth: !!res.token,
-                                error: ""
-                            }));
-                        }
-                //     }
-                // }
+                    return res as z.infer<LoginClientData['response']>;
+                } else {
+                    set((state) => ({ 
+                        token: res.token,
+                        firstname: res.firstname,
+                        isAuth: !!res.token,
+                        error: ""
+                    }));
+                }
 
                 return;
             },
